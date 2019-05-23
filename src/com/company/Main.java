1f53b2c41b8c33116ea956be146aad16b304f543
LuciaGojeiu <lucia.gojeiu@gmail.com>
2019-05-23 20:48:22 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+     int myInt=1234;
+     System.out.println(myInt);
+     byte x=2;
+     x=x++;
+     System.out.println(++x);
+    }
+}
