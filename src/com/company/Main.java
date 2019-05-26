cb9b74b5c83a451459bad3051400d710f78bbdf2
LuciaGojeiu <lucia.gojeiu@gmail.com>
2019-05-26 21:59:26 +0300

    Commit tema

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    //Printare "Hello" in consola, iar pe rand nou numele:
+      //Printare "Hello" in consola, iar pe rand nou numele:
       System.out.println("Hello");
       System.out.println("Lucia");
 
